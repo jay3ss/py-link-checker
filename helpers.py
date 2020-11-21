330a386e71bef01bd52b899d1a88f616f36bbe9d
jay ess <mentoce@gmail.com>
2020-11-21 02:06:59 -0800

    Add missing '\r' character that caused the cursor to fail to return to the beginning of the line

--- helpers.py
+++ helpers.py
@@ -9,6 +9,6 @@ def print_same_line(*msg):
 
     Got some help from https://stackoverflow.com/a/5419488/3562890
     """
-    clear = '\x1b[1K'
+    clear = '\x1b[1K\r'
     print(clear, *msg, end='', flush=True, sep='')
 
